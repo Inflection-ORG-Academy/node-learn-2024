52d4a50049f9825e9bdcbafc62aa982373a6c709
Prakhar Maurya <000.excel@gmail.com>
2025-07-07 07:50:50 +0530

    init

---
+++ middleware.mjs
@@ -0,0 +1,13 @@
+const globalMiddleware = (req, res, next) => {
+  req.name = "apple"
+  console.log("global middleware apple")
+  next();
+}
+
+
+const localMiddleware = (req, res, next) => {
+  console.log("local files middleware")
+  next()
+}
+
+export { globalMiddleware, localMiddleware }
