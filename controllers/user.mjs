f5fbe01d0eb830c07c21fde56a83f7fa27df8a4e
Prakhar Maurya <000.excel@gmail.com>
2025-07-08 07:55:48 +0530

    Implement user registration functionality and add initial user data

--- controllers/user.mjs
+++ controllers/user.mjs
@@ -1,5 +1,24 @@
-const registerController = (req, res, next) => {
-  res.json({ message: "register not implemented" })
+import { readFile, writeFile } from 'node:fs/promises'
+
+const registerController = async (req, res, next) => {
+  if (!req.body.name || !req.body.email || !req.body.password) {
+    res.statusCode = 400
+    res.json({ error: "input is not valid" })
+    // throw new Error(JSON.stringify({ error: "input is not valid" }))
+  }
+
+  const fileDataStr = await readFile("./db.json", {
+    encoding: 'utf-8'
+  })
+  const fileData = JSON.parse(fileDataStr)
+  const userData = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  }
+  fileData.users.push(userData)
+  await writeFile("./db.json", JSON.stringify(fileData))
+  res.json({ message: "register successful" })
 }
 
 const loginController = (req, res, next) => {
