b535fbcdacf1b81247ab50ec1e8396783e62daef
Prakhar Maurya <000.excel@gmail.com>
2025-07-21 08:23:57 +0530

    Add reset password functionality with token validation and password update

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 import express from 'express';
-import { registerController, loginController, forgotPasswordController } from './controllers/user.mjs'
+import { registerController, loginController, forgotPasswordController, resetPasswordController } from './controllers/user.mjs'
 import { prizeController } from './controllers/prize.mjs'
 import { globalMiddleware, authMiddleware } from './middleware.mjs';
 import { errorController, undefinedRouteHandler } from './error.mjs';
@@ -14,6 +14,7 @@ server.use(globalMiddleware)
 server.post("/register", registerController);
 server.post("/login", loginController);
 server.post("/forgot_password", forgotPasswordController);
+server.patch("/reset_password/:token", resetPasswordController);
 server.get("/prize", authMiddleware, prizeController)
 
 // Catch-all route
